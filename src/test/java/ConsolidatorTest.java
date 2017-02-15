27bd76b8b35e5aaf5beaaef429d49c7ecd192031
simplay <silent.simplay@gmail.com>
2017-02-15 16:16:52 +0100

    Wrote more tests

--- src/test/java/ConsolidatorTest.java
+++ src/test/java/ConsolidatorTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ConsolidatorTest {
 
@@ -41,5 +42,64 @@ public class ConsolidatorTest {
         assertEquals(src.getCellValue(2,0), excel.getCellValue(2, 0));
         assertEquals(src.getCellValue(2,1), excel.getCellValue(2, 1));
         assertEquals(src.getCellValue(2,2), excel.getCellValue(2, 2));
+
+        assertNotEquals(src.getCellValue(0,0), excel.getCellValue(0, 1));
+    }
+
+    @Test
+    public void testCopyConstantValuesToOtherExcel() {
+        String[] args = {
+                TestHelper.getReadOnlyFilePath(),
+                TestHelper.getWritableFilePath(),
+                TestHelper.getMappingFilePath()
+        };
+        Properties.initialize(args);
+
+        ExcelFile src = new ExcelFile(Properties.getFromExcelFilePath(), 0);
+        ExcelFile dst = new ExcelFile(Properties.getToExcelFilePath(), 0);
+        new Consolidator("src/test/data/mappingsWithConstant.txt", src, dst);
+        dst.save();
+
+        ExcelFile excel = new ExcelFile(TestHelper.getWritableFilePath(), 0);
+        assertEquals("foobar1", excel.getCellValue(0, 0));
+        assertEquals("foobar2", excel.getCellValue(0, 1));
+        assertEquals("foobar3", excel.getCellValue(0, 2));
+        assertEquals("foobar4", excel.getCellValue(1, 0));
+        assertEquals("foobar5", excel.getCellValue(1, 1));
+        assertEquals("foobar6", excel.getCellValue(1, 2));
+        assertEquals("foobar7", excel.getCellValue(2, 0));
+        assertEquals("foobar8", excel.getCellValue(2, 1));
+        assertEquals("foobar9", excel.getCellValue(2, 2));
+
+        assertNotEquals("foobar1", excel.getCellValue(0, 1));
+    }
+
+    // TODO test growing columns
+    // TODO test with mixed cases
+
+    @Test
+    public void testCopyWithScalaTranslation() {
+        String[] args = {
+                "src/test/data/scala_values.xlsx",
+                TestHelper.getWritableFilePath(),
+                "src/test/data/mappingsWithScala.txt",
+                "src/test/data/scala_values.txt"
+        };
+        Properties.initialize(args);
+        ExcelFile src = new ExcelFile(Properties.getFromExcelFilePath());
+        ExcelFile dst = new ExcelFile(Properties.getToExcelFilePath());
+        new Consolidator(Properties.getMappingFilePath(), src, dst);
+        dst.save();
+
+        ExcelFile excel = new ExcelFile(TestHelper.getWritableFilePath(), 0);
+        assertEquals("1", excel.getCellValue(0, 0));
+        assertEquals("2", excel.getCellValue(0, 1));
+        assertEquals("3", excel.getCellValue(0, 2));
+        assertEquals("10", excel.getCellValue(1, 0));
+        assertEquals("11", excel.getCellValue(1, 1));
+        assertEquals("12", excel.getCellValue(1, 2));
+        assertEquals("23", excel.getCellValue(2, 0));
+        assertEquals("34", excel.getCellValue(2, 1));
+        assertEquals("45", excel.getCellValue(2, 2));
     }
 }
